70081a8ceb134d86952914a9d8eb3a6e12624310
Brazuca <gbruzacac@gmail.com>
2024-07-31 10:49:05 -0300

    staging - experiment voila

--- indicators/indicadores_pedidos.py
+++ indicators/indicadores_pedidos.py
@@ -7,10 +7,9 @@ class OrderIndicators:
     def __init__(self, profile):
         self.consolidado_report = PostgresConsolidatedRepository(profile=profile)
 
-    def get_total_orders(self) -> pandas.DataFrame:
-        data = self.consolidado_report.order_indicators_per_client()
+    def get_total_orders(self, is_billed: bool = False) -> pandas.DataFrame:
+        data = self.consolidado_report.order_indicators_per_client(is_billed=is_billed)
         orders_by_distributor = pandas.DataFrame(data)
-        print(orders_by_distributor)
         return orders_by_distributor
 
 
